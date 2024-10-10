4871220fb79a10789cf42720fadb665d33d1df2d
Cole Nelson <ctnelson1997@gmail.com>
2024-10-10 11:24:45 -0500

    ice js 3

---
+++ starter/app.js
@@ -0,0 +1,40 @@
+console.log("Hello world!")
+
+/**
+ * Grabs the numbers and operations from document, performs the calculation,
+ * and alerts the user of the result. If any of the inputs are empty, it will
+ * alert the user to complete the form instead.
+ */
+function calculate() {
+    alert("I should do a calculation!")
+}
+
+/**
+ * Gets the number of an input if it exists, otherwise returns null
+ * @param {string} name the ID of the HTML element
+ * @returns {number || null} the input as a number, or null if empty
+ */
+function getNumberInput(name) {
+
+}
+
+/**
+ * Alerts the user which, if any, of the input fields are missing.
+ * @param {string || null} num1 the first input field value
+ * @param {string || null} num2 the second input field value
+ * @returns {undefined} does not have a return
+ */
+function alertEmpty(num1, num2) {
+
+}
+
+/**
+ * Returns the result of x and y based on the operation
+ * @param {number} x The first operand
+ * @param {number} y The second operand
+ * @param {string} operation The operator
+ * @returns {number} the result
+ */
+function getResult(x, y, operation) {
+    
+}
